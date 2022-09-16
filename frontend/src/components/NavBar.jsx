6dcbcb8212678a50f7030e6df46591f50adc5b8d
Sayyed-Salman <sayyedsalman2000@gmail.com>
2022-09-16 21:13:25 +0530

    initial commit

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,84 @@
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import styled from "styled-components";
+import { logoutUser } from "../slices/authSlice";
+import { toast } from "react-toastify";
+
+const NavBar = () => {
+  const dispatch = useDispatch();
+  const { cartTotalQuantity } = useSelector((state) => state.cart);
+  const auth = useSelector((state) => state.auth);
+
+  console.log(auth);
+
+  return (
+    <nav className="nav-bar">
+      <Link to="/">
+        <img src="https://raw.githubusercontent.com/STQA-Project/MoboKart/2341e22338f64d615164445960f0a9a5f033e10c/designs/login_signup/mobokartsvg.svg?token=AR4276VGAOV7SFKQAY57AZ3DENZTE" alt="logo" width="100" />
+        <h1>MoboKart</h1>
+      </Link>
+      <Link to="/cart">
+        <div className="nav-bag">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="35"
+            height="35"
+            fill="currentColor"
+            className="bi bi-handbag-fill"
+            viewBox="0 0 16 16"
+          >
+            <path d="M8 1a2 2 0 0 0-2 2v2H5V3a3 3 0 1 1 6 0v2h-1V3a2 2 0 0 0-2-2zM5 5H3.36a1.5 1.5 0 0 0-1.483 1.277L.85 13.13A2.5 2.5 0 0 0 3.322 16h9.355a2.5 2.5 0 0 0 2.473-2.87l-1.028-6.853A1.5 1.5 0 0 0 12.64 5H11v1.5a.5.5 0 0 1-1 0V5H6v1.5a.5.5 0 0 1-1 0V5z" />
+          </svg>
+          <span className="bag-quantity">
+            <span>{cartTotalQuantity}</span>
+          </span>
+        </div>
+      </Link>
+      {auth._id ? (
+        <Links>
+          {auth.isAdmin ? (
+            <div>
+              <Link to="/admin/summary">Admin</Link>
+            </div>
+          ) : null}
+          <div
+            onClick={() => {
+              dispatch(logoutUser(null));
+              toast.warning("Logged out!", { position: "bottom-left" });
+            }}
+          >
+            Logout
+          </div>
+        </Links>
+      ) : (
+        <AuthLinks>
+          <Link to="/login">Login</Link>
+          <Link to="register">Register</Link>
+        </AuthLinks>
+      )}
+    </nav>
+  );
+};
+
+export default NavBar;
+
+const AuthLinks = styled.div`
+  a {
+    &:last-child {
+      margin-left: 2rem;
+    }
+  }
+`;
+
+const Links = styled.div`
+  color: white;
+  display: flex;
+
+  div {
+    cursor: pointer;
+
+    &:last-child {
+      margin-left: 2rem;
+    }
+  }
+`;
---
+++ readme.md
@@ -0,0 +1,34 @@
+# MoboKart v1
+
+## How to run
+
+- frontend
+
+  `cd frontend`
+  <br>
+  `npm install`
+  <br>
+  `npm start`
+
+- backend
+
+  `cd backend`
+  <br>
+  `npm install`
+  <br>
+  `node index.js`
+
+<hr>
+
+## ENV file
+
+```
+DB_URI = "mongodb+srv://epicd3055:newpassword@mobokart.9rj7lnx.mongodb.net/?retryWrites=true&w=majority"
+JWT_SECRET_KEY = <>
+STRIPE_KEY = sk_test_51Lig01SHdO3ee1PlCHMtkf2NJysc3EoBsJYUCqrG7YaGrif48QJcl1CUMlepQhYV2sWypgYOQd1rvSK5Tp6pu0eQ00XYd4emkv
+STRIPE_WEB_HOOK = <>
+CLIENT_URL = http://localhost:3000
+CLOUDINARY_NAME = dqm7ig7oi
+CLOUDINARY_API_KEY = 685974744966867
+CLOUDINARY_API_SECRET = 5unbQryu8g90KUC4MjJ93bCT0Nk
+```
